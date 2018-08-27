e99d474da21ab280760997eee741f1b5545b4ce1
Batian <bmuthoga@gmail.com>
2018-08-27 09:33:02 +0300

    feat(add-utility-helpers): add utility helpers
    - add 'getFunName()' helper function

--- scripts/main.js
+++ scripts/main.js
@@ -7,6 +7,8 @@ var Route = ReactRouter.Route;
 var Navigation = ReactRouter.Navigation;
 var createBrowserHistory = require('history/lib/createBrowserHistory');
 
+var helperFuncs = require('./helpers');
+
 /* 
   App
 */
@@ -81,7 +83,7 @@ var StorePicker = React.createClass({
     return (
       <form className="store-selector">
         <h2>Please Enter A Store</h2>
-        <input type="text" ref="storeId" required />
+        <input type="text" ref="storeId" required defaultValue={helperFuncs.getFunName()} />
         <input type="submit" />
       </form>
     );
